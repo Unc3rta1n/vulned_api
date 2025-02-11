114f7ecd6ee669301b6075e347eedae659675ce5
yamatin_nikita <yamatin_nikita@cybersec74.ru>
2025-02-11 16:08:38 +0500

    added routes, and 2 variants for query

---
+++ logger.py
@@ -0,0 +1,43 @@
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "[%(name)s] [%(asctime)s] %(levelname)s - [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt="%d.%m.%y-%H:%M:%S")
+        return formatter.format(record)
+
+
+def setup_logger(name: str, level: str) -> logging.Logger:
+
+    formatter = CustomFormatter()
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    logger.handlers = []
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+logger = setup_logger("logger", logging.INFO)
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from fastapi import FastAPI
+import asyncpg
+from logger import logger
+from db import get_conn_db, _create_user, _good_create_user
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/bad_create_user")
+async def create_user(f_name:str, l_name:str, password:str):
+    conn = await get_conn_db()
+    logger.info(f"{conn =} {type(conn)}")
+    result = await _create_user(conn, f_name, l_name, password)
+    if result is None:
+        return {"message": "Something went wrong"}
+    else:
+        return {"message": "User created successfully"}
+
+
+@app.post("/good_create_user")
+async def create_user(f_name:str, l_name:str, password:str):
+    conn = await get_conn_db()
+    logger.info(f"{conn =} {type(conn)}")
+    result = await _good_create_user(conn, f_name, l_name, password)
+    if result is None:
+        return {"message": "Something went wrong"}
+    else:
+        return {"message": "User created successfully"}
+@app.get("/users")
+async def get_users():
+    conn = await get_conn_db()
+    logger.info(f"{conn =} {type(conn)}")
+    user_list = await _get_users(conn)
